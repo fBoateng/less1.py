6f879a10a041a8918fe73b235c679aa0074954c6
Francis Boateng <b0af@ymail.com>
2023-06-06 14:15:55 +0000

    django

--- Django/monthly_challenges/challenges/views.py
+++ Django/monthly_challenges/challenges/views.py
@@ -1,29 +1,38 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+
+monthly_challenges = {
+    'january': 'This is January!',
+    'february': 'This is February',
+    'march': 'This is March!',
+    'april': 'This is April',
+    'may': 'This is May!',
+    'june': 'This is June',
+    'july': 'This is July!',
+    'august': 'This is August',
+    'september': 'This is September!',
+    'october': 'This is October',
+    'november': 'This is November!',
+    'december': 'This is December',   
+}
 
 
 # Create your views here.
-'''def january(request):
-    return HttpResponse('for january')
-
-
-def february(request):
-    return HttpResponse('for february')
-
-def march(request):
-    return HttpResponse('for march')'''
+def monthly_challenge_by_number(request, month):
+    months = list(monthly_challenges.keys())
+    
+    if month > len(months):
+        return HttpResponseNotFound('There are 12 months please enter 1.')
+    
+    forward_month = months[month-1]
+    return HttpResponseRedirect('/challenges/' + forward_month)
 
 
 def monthly_challenge(request, month):
-    challenge_text = None
-    if month == 'january':
-        challenge_text = 'This is January!'
-    elif month == 'february':
-        challenge_text = 'This is February!'
-    elif month == 'march':
-        challenge_text = 'This is march!'
-    else:
-        return HttpResponseNotFound('Too far ahead we are not there yet.!')
+    try:
+        challenge_text = monthly_challenges[month]
+    except:
+        return HttpResponseNotFound('Month not found!')
     return HttpResponse(challenge_text)
 
 
