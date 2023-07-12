29c71f5a2b63b8cf6c17136cffc7bec4c6606303
fBoateng <b0af@ymail.com>
2023-07-12 11:46:31 +0000

    commit

--- library/music_store/outlet/models.py
+++ library/music_store/outlet/models.py
@@ -1,9 +1,13 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
 class Song(models.Model):
     title = models.CharField(max_length=50)
-    rating = models.IntegerField()
+    rating = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(5)])
+    is_bestselling = models.BooleanField(default=False)
+    singer = models.CharField(null=True, max_length=100)
     
     
     
