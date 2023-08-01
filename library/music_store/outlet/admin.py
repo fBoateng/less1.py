165b53dbeeef1a1a722a85965609941c52eebc50
fboateng <b0af@ymail.com>
2023-08-01 15:24:19 +0000

    damage

--- library/music_store/outlet/admin.py
+++ library/music_store/outlet/admin.py
@@ -3,4 +3,10 @@ from django.contrib import admin
 from .models import Song
 
 # Register your models here.
-admin.site.register(Song)
+
+class SongAdmin(admin.ModelAdmin):
+    readonly_fields = ('slug',)
+    prepopulated_fields = {'slug': ('title')}
+
+
+admin.site.register(Song, SongAdmin)
--- library/music_store/outlet/models.py
+++ library/music_store/outlet/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
+from django.utils.text import slugify
 
 # Create your models here.
 class Song(models.Model):
@@ -8,8 +9,14 @@ class Song(models.Model):
         validators=[MinValueValidator(1), MaxValueValidator(5)])
     is_bestselling = models.BooleanField(default=False)
     singer = models.CharField(null=True, max_length=100)
+    slug = models.SlugField(default='', null=False)
+
     
-    
+def save(self, *args,  **kwargs) :
+    self.slug = slugify(self.title)
+    super().save(*args, **kwargs)
+
+       
     
 def __str__(self):
     return f'{self.title} ({self.rating})'
